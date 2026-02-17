9d33c02acb8b441284cb07478a9626ac158bfce3
SatoshiTomita <S.Tomita2023@keio.jp>
2026-02-17 07:06:45 +0000

    1-3でーたをprint

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from kenshu!")
+import numpy as np
+import matplotlib.pyplot as plt
 
+data = np.array([[1,2,3], [4,5,6], [7,8,9]]) # ただの一例です
 
-if __name__ == "__main__":
-    main()
+min_value = 0
+max_value = 2 * np.pi
+points_per_round = 100 # １周あたり何個の点のデータにするか
+num_rounds = 5 # 何周分のデータにするか
+# (points_per_round * num_rounds) 個のデータになる
+theta = np.linspace(min_value, max_value * num_rounds, points_per_round * num_rounds)
+
+print(theta) # データの中身がそのまま出てきます
+print(theta.shape) # (points_per_round * num_rounds,) (上記の設定で書いたら250)
