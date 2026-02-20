5394ffa803bbb3d1dd60221c072134b5151ceb9a
SatoshiTomita <S.Tomita2023@keio.jp>
2026-02-20 23:46:03 +0900

    1-8復習

--- kadai1/1-7/src/trainer/trainer.py
+++ kadai1/1-7/src/trainer/trainer.py
@@ -32,7 +32,7 @@ class Trainer():
 
             minibatch_val_loss = validation_loop(self.model, self.val_dataloader, self.loss_fn)
             validation_loss_log.append(minibatch_val_loss)
-            
+            # wandbでグラフ化
             wandb.log({"train_loss": minibatch_train_loss,
             "validation_loss": minibatch_val_loss,
             "epoch": e,
@@ -45,27 +45,37 @@ class Trainer():
 
 
 def train_loop(model, train_dataloader, optimizer, loss_fn):
+    # trainループの初期化
     model.train()
     minibatch_train_loss = 0
+    # dataloaderからデータを取り出す
     for i, batch in enumerate(train_dataloader):
         input, target = batch
+        # inputをモデルに入れる
         prediction = model(input)
+        # モデルの出力と正解を損失関数に入れる
         train_loss = loss_fn(prediction, target)
 
+        # 勾配をリセット
         optimizer.zero_grad()
+        # 改めて勾配を計算
         train_loss.backward()
+        # パラメータ更新
         optimizer.step()
 
+        # 損失の和を計算
         minibatch_train_loss += train_loss.item()
-
+    # index分で損失を割り平均的な損失を算出する
     minibatch_train_loss /= (i+1)
     return minibatch_train_loss
 
 def validation_loop(model, val_dataloader, loss_fn):
+    # model.evelでモードを変更
     model.eval()
     minibatch_val_loss = 0
     with torch.no_grad():
         for j, batch in enumerate(val_dataloader):
+            # パラメータの更新や勾配計算んを行わない
             input, target = batch
             prediction = model(input)
             validation_loss = loss_fn(prediction, target)
